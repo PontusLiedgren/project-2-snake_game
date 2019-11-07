5f484979d583060f506b97026a744b7747e37d93
Pontus Liedgren <pop2strong4u@gmail.com>
2019-11-07 11:53:28 +0100

    Input and lists

--- python-passwordgenerator.py
+++ python-passwordgenerator.py
@@ -1,4 +1,18 @@
 # project-2-passwordgenerator
 #
-# Author Pontus Liedgren
+# Author: Pontus Liedgren
 # https://github.com/PontusLiedgren/project-2-passwordgenerator
+
+
+# Password generator
+
+import random
+
+name_input = input("Välkommen till lösenordsgeneratorn!\n" + "Säg ditt namn: ")
+birth_input = input("Säg ditt födelsedatum(åååå-mm-dd ex: 1979-02-24): ")
+interest_input = input("Säg något intresse: ")
+
+password_list = [name_input, birth_input, interest_input]
+
+
+
