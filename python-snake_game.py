96592c380662f965bdb148dda47fffaeecb66de4
Pontus Liedgren <pop2strong4u@gmail.com>
2019-11-18 10:17:00 +0100

    Initial commit

--- python-snake_game.py
+++ python-snake_game.py
@@ -6,5 +6,18 @@
 
 # Snake game
 
+# imports
+import pygame   
 
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+done = False
 
+while not done:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        done = True
+        
+        pygame.display.flip()
+
+pygame.draw.rect(screen, (30, 128, 255), pygame.Rect(30, 30, 60, 60))
