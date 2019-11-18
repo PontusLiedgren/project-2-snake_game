c2da2a747e60d35e2c5fb54e3677e248daac0a77
Pontus Liedgren <pop2strong4u@gmail.com>
2019-11-18 09:52:29 +0100

    Changed project

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# project-2-passwordgenerator
+# project-2-snake_game
 
 Game in Python 3.
---
+++ python-snake_game.py
@@ -0,0 +1,10 @@
+# project-2-snake
+#
+# Author: Pontus Liedgren
+# https://github.com/PontusLiedgren/project-2-snake_game
+
+
+# Snake game
+
+
+
