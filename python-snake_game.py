7ee013a15e34b163d105f7e46531c38c5e5acb1a
Vilhelm Prytz <vilhelm@prytznet.se>
2019-12-01 21:38:43 +0100

    Re-format using code-formatter black

--- python-snake_game.py
+++ python-snake_game.py
@@ -10,10 +10,11 @@
 import pygame, random
 
 from snake import Snake
+
 pygame.init()
 
 GREEN = (76, 187, 40)
-GREY =  (210, 210, 210)
+GREY = (210, 210, 210)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 PURPLE = (255, 0, 255)
@@ -29,8 +30,8 @@ pygame.display.set_caption("Snekgem")
 all_sprites_list = pygame.sprite.Group()
 
 playerSnake = Snake(BLUE, 30, 30)
-playerSnake.rect.x = SCREENWIDTH/2
-playerSnake.rect.y = SCREENHEIGHT/2
+playerSnake.rect.x = SCREENWIDTH / 2
+playerSnake.rect.y = SCREENHEIGHT / 2
 
 
 all_sprites_list.add(playerSnake)
@@ -38,32 +39,35 @@ all_sprites_list.add(playerSnake)
 carryOn = True
 clock = pygame.time.Clock()
 
+
 def steering(player):
-        if keys[pygame.K_LEFT]:
-                player.rect.y += 0
-                player.rect.x -= 1
-        if keys[pygame.K_RIGHT]:
-                player.rect.y += 0
-                player.rect.x += 1
-        if keys[pygame.K_UP]:
-                player.rect.x += 0
-                player.rect.y -= 1
-        if keys[pygame.K_DOWN]:
-                player.rect.x += 0 
-                player.rect.y += 1
+    if keys[pygame.K_LEFT]:
+        player.rect.y += 0
+        player.rect.x -= 1
+    if keys[pygame.K_RIGHT]:
+        player.rect.y += 0
+        player.rect.x += 1
+    if keys[pygame.K_UP]:
+        player.rect.x += 0
+        player.rect.y -= 1
+    if keys[pygame.K_DOWN]:
+        player.rect.x += 0
+        player.rect.y += 1
+
+
 while carryOn:
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                        carryOn = False
-                elif event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_x:
-                                carryOn = False
-        keys = pygame.key.get_pressed()
-        steering(playerSnake)
-        all_sprites_list.update()
-        screen.fill(GREEN)
-        all_sprites_list.draw(screen)
-        pygame.display.flip()
-        clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            carryOn = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_x:
+                carryOn = False
+    keys = pygame.key.get_pressed()
+    steering(playerSnake)
+    all_sprites_list.update()
+    screen.fill(GREEN)
+    all_sprites_list.draw(screen)
+    pygame.display.flip()
+    clock.tick(60)
 
 pygame.quit()
--- snake.py
+++ snake.py
@@ -1,21 +1,28 @@
 import pygame
+
 WHITE = (255, 255, 255)
 
-class Snake(pygame.sprite.Sprite):
-        speed = 3
-        def __init__(self, color, width, height):
-                super().__init__()
 
-                self.image = pygame.Surface([width, height])
-                self.image.fill(WHITE)
-                self.image.set_colorkey(WHITE)
-                pygame.draw.rect(self.image, color, [0, 0, width, height])
-                self.rect = self.image.get_rect() 
-        def moveRight(self):
-            self.rect.x = self.rect.x + self.speed
-        def moveLeft(self):
-            self.rect.x = self.rect.x - self.speed
-        def moveUp(self):
-            self.rect.y = self.rect.y - self.speed
-        def moveDown(self):
-            self.rect.y = self.rect.y + self.speed
+class Snake(pygame.sprite.Sprite):
+    speed = 3
+
+    def __init__(self, color, width, height):
+        super().__init__()
+
+        self.image = pygame.Surface([width, height])
+        self.image.fill(WHITE)
+        self.image.set_colorkey(WHITE)
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        self.rect = self.image.get_rect()
+
+    def moveRight(self):
+        self.rect.x = self.rect.x + self.speed
+
+    def moveLeft(self):
+        self.rect.x = self.rect.x - self.speed
+
+    def moveUp(self):
+        self.rect.y = self.rect.y - self.speed
+
+    def moveDown(self):
+        self.rect.y = self.rect.y + self.speed
